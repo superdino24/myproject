e0ad2c81a9c565d1156e8e2c9883b7279554877d
dinosmac <superdino24@hotmail.com>
2020-08-11 23:32:33 +0800

    Jave and Kotlin Variable,data type

--- src/main/java/com/dino/Hello.java
+++ src/main/java/com/dino/Hello.java
@@ -1,11 +1,23 @@
 package com.dino;
 
+import sun.jvm.hotspot.types.CIntegerField;
+
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello worlddddddd");
 //        new Person().hello();
         Person p = new Person();
         p.hello();
+        int age = 18;
+        Integer age2 = 18;
+        char c ='A';
+        Character c2='A';
+        byte b = 120;
+        float weight = 66.5f;
+        double height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Dino";
 
     }
 }
--- src/main/kotlin/com/dino/Hello.kt
+++ src/main/kotlin/com/dino/Hello.kt
@@ -1,11 +1,15 @@
 package com.dino
 
-fun main() {
+fun main(args: Array<String>) {
 //    println("Hello kotlin");
 //    Human().hello()
     val h = Human()
-    h.hello()
-
+        h.hello()
+    var age =19;
+    age = 20;
+    var weight = 66.5;
+    var name : String
+    name = "Dino"
 }
 
 class Human {
